fc399fda8a6b104d3d102a5177b32d32c4f4a80a
Dmitriano <winappdev@gmail.com>
2024-09-23 10:19:18 -0700

    implemented YMAInterstitialAdLoaderDelegate

--- ad/Ad.h
+++ ad/Ad.h
@@ -1,10 +1,17 @@
 #pragma once
 
+@class AdImpl;
+
 namespace AdExample
 {
     class Ad
     {
     public:
+
         Ad();
+
+    private:
+
+        AdImpl* m_impl;
     };
 }
--- ad/Ad.mm
+++ ad/Ad.mm
@@ -2,18 +2,34 @@
 
 #import <YandexMobileAds/YandexMobileAds-Swift.h>
 
+using namespace AdExample;
+
 // AbstractClass.h 
-@interface AbstractClass : NSObject
+@interface AdImpl <YMAInterstitialAdLoaderDelegate>
 - (void)method1; 
 - (void)method2; 
 @end
 // AbstractClass.m 
-@implementation AbstractClass 
+@implementation AdImpl 
 - (void)method1 { 
    NSLog(@"Method 1"); 
    YMAInterstitialAdLoader *loader = [[YMAInterstitialAdLoader alloc]init];
+   loader.delegate = self;
 } 
 - (void)method2 { 
    NSLog(@"Method 2"); 
 } 
+
+- (void)interstitialAdLoader:(YMAInterstitialAdLoader * _Nonnull)adLoader didLoad:(YMAInterstitialAd * _Nonnull)interstitialAd {
+}
+
+
+- (void)interstitialAdLoader:(YMAInterstitialAdLoader * _Nonnull)adLoader didFailToLoadWithError:(YMAAdRequestError * _Nonnull)error {
+
+}
 @end
+
+Ad::Ad()
+{
+    m_impl = [[AdImpl alloc]init];
+}
